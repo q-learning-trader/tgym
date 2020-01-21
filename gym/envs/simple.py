0fc242d879cc07577c35e54ef87b5f19f5827ccd
wen <liuwen.yzu@gmail.com>
2020-01-21 18:28:27 +0800

    init

---
+++ gym/envs/simple.py
@@ -0,0 +1,21 @@
+# -*- coding:utf-8 -*-
+"""
+单支股票全量日内买卖
+action: [sell_price, buy_price]
+"""
+
+import gym
+
+
+class SimpleEnv(gym.Env):
+    def __init__(self):
+        pass
+
+    def reset(self):
+        pass
+
+    def step(self, action):
+        pass
+
+    def render(self, mode='human'):
+        pass
