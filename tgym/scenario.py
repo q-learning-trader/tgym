a52ff2a20789d5a010e18e31bea12190b7d47888
iminders <liuwen.w@qq.com>
2020-02-07 17:18:03 +0800

    scenario

--- tgym/scenario.py
+++ tgym/scenario.py
@@ -1,13 +1,15 @@
 # -*- coding:utf-8 -*-
-import tgym.envs as envs
+from tgym.envs.average import AverageEnv
+from tgym.envs.multi_vol import MultiVolEnv
+from tgym.envs.simple import SimpleEnv
 
 
 def make_env(scenario="", market=None, investment=100000.0, look_back_days=10):
     if scenario == "simple":
-        return envs.simple.SimpleEnv(market, investment, look_back_days)
+        return SimpleEnv(market, investment, look_back_days)
     elif scenario == "average":
-        return envs.average.AverageEnv(market, investment, look_back_days)
+        return AverageEnv(market, investment, look_back_days)
     elif scenario == "multi_vol":
-        return envs.multi_vol.MultiVolEnv(market, investment, look_back_days)
+        return MultiVolEnv(market, investment, look_back_days)
     else:
         raise "Not implement scenario %S" % scenario
